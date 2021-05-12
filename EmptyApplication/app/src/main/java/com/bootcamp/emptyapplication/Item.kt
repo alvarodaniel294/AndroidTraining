c0a33bc121dd9050a19cc388756b0703e30d40cf
Kevin Viscafe <kevinviscafe@gmail.com>
2021-05-12 13:58:31 -0400

    First Commit

---
+++ EmptyApplication/app/src/main/java/com/bootcamp/emptyapplication/Item.kt
@@ -0,0 +1,3 @@
+package com.bootcamp.emptyapplication
+
+data class Item()
---
+++ EmptyApplication/app/src/main/java/com/bootcamp/emptyapplication/MyItemRecyclerViewAdapter.kt
@@ -0,0 +1,49 @@
+package com.bootcamp.emptyapplication
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+
+import com.bootcamp.emptyapplication.placeholder.PlaceholderContent.PlaceholderItem
+import com.bootcamp.emptyapplication.databinding.FragmentItemBinding
+
+/**
+ * [RecyclerView.Adapter] that can display a [PlaceholderItem].
+ * TODO: Replace the implementation with code for your data type.
+ */
+class MyItemRecyclerViewAdapter(
+    private val values: List<PlaceholderItem>
+) : RecyclerView.Adapter<MyItemRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(
+            FragmentItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        holder.show(item)
+    }
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(binding: FragmentItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        val idView: TextView = binding.title
+        val imageView: ImageView = binding.imageView
+
+        fun show(item: PlaceholderItem) {
+            idView.text =
+        }
+    }
+
+}
