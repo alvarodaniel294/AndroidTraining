9cf0394a6c45af2ac4f304f2243d03dfdf7b764b
Christian Torrico <christorricoavila@gmail.com>
2021-05-12 13:57:17 -0400

    adding code

--- EmptyApplication/app/src/main/java/com/bootcamp/emptyapplication/MainActivity.kt
+++ EmptyApplication/app/src/main/java/com/bootcamp/emptyapplication/MainActivity.kt
@@ -2,11 +2,20 @@ package com.bootcamp.emptyapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.bootcamp.emptyapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val text = "Hola chris"
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val bottomNav = binding.mainBottomNavigation
+        val navController = findNavController(R.id.fragment)
+        bottomNav.setupWithNavController(navController)
     }
 }
