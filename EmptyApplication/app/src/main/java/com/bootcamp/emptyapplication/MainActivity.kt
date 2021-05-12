2e896ff4321dfb08aa6457aaeb2b2c6700ad5334
Andres.Arana <andres.arana@jala-foundation.org>
2021-05-12 13:23:07 -0400

    navigation implemented

--- EmptyApplication/app/src/main/java/com/bootcamp/emptyapplication/MainActivity.kt
+++ EmptyApplication/app/src/main/java/com/bootcamp/emptyapplication/MainActivity.kt
@@ -2,10 +2,21 @@ package com.bootcamp.emptyapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment: NavHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        val navigation = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomView)
+
+        bottomNavigationView.setupWithNavController(navigation)
     }
 }
