358ba2fe2fcd74b19ff4debd43418fd9cc1d1e51
Alvaro Saavedra <asaavedra@ecisolutions.com>
2021-05-14 17:27:12 -0400

    Add livedata project

---
+++ LiveDataExample/app/src/main/java/com/bootcamp/livedataexample/TopFragment.kt
@@ -0,0 +1,43 @@
+package com.bootcamp.livedataexample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import com.bootcamp.livedataexample.viewmodels.MainActivityViewModel
+
+
+class TopFragment : Fragment() {
+
+
+    private val viewModel:MainActivityViewModel by activityViewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_top, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val textView = view.findViewById<TextView>(R.id.textViewTopFragment)
+
+        viewModel.title.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+    }
+}
