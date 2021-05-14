9399b5fa22c8eb14ad2a6277968d9b6d8b11b5a4
Christian Torrico <christorricoavila@gmail.com>
2021-05-14 08:47:56 -0400

    adding siwpe refresh layout

--- EmptyApplication/app/src/main/java/com/bootcamp/emptyapplication/HomeFragment.kt
+++ EmptyApplication/app/src/main/java/com/bootcamp/emptyapplication/HomeFragment.kt
@@ -1,14 +1,19 @@
 package com.bootcamp.emptyapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
-class HomeFragment : Fragment() {
+class   HomeFragment : Fragment() {
+
+    lateinit var recyclerAdapter: DogAdapter
     var dogs = mutableListOf<String>(
             "https://images.dog.ceo/breeds/hound-basset/n02088238_9626.jpg",
             "https://images.dog.ceo/breeds/mexicanhairless/n02113978_2425.jpg",
@@ -20,15 +25,35 @@ class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view =  inflater.inflate(R.layout.fragment_home, container, false)
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val rv_RecycleView: RecyclerView = view.findViewById(R.id.rv_dogs)
         rv_RecycleView.layoutManager = LinearLayoutManager(parentFragment?.context) //HomeFragment().context
-        val recyclerAdapter = DogAdapter(dogs)
+        recyclerAdapter = DogAdapter(dogs)
         rv_RecycleView.adapter = recyclerAdapter
         recyclerAdapter.notifyDataSetChanged()
+        initSwipeRefresher(view)
     }
+
+    fun initSwipeRefresher(view: View){
+        Log.d("chris","entro adentro")
+        val swipeRefreshLayout = view.findViewById<SwipeRefreshLayout>(R.id.home_swipe_refresh)
+        swipeRefreshLayout.setOnRefreshListener {
+            Toast.makeText(view.context,"Se empezo a refrescar la pag", Toast.LENGTH_SHORT).show()
+            dogs.add("https://images.dog.ceo/breeds/hound-basset/n02088238_9626.jpg")
+            dogs.add("https://images.dog.ceo/breeds/hound-basset/n02088238_9626.jpg")
+            dogs.add("https://images.dog.ceo/breeds/hound-basset/n02088238_9626.jpg")
+            recyclerAdapter.notifyDataSetChanged()
+            swipeRefreshLayout.isRefreshing = false
+        }
+    }
+
+    fun didSelectImage(){
+
+    }
+
 }
