7327a677e65ee2eda7e41ac964441e7fd952f602
Andres.Arana <andres.arana@jala-foundation.org>
2021-05-13 23:37:10 -0400

    Configured refresh layout

--- EmptyApplication/app/src/main/java/com/bootcamp/emptyapplication/ListFragment.kt
+++ EmptyApplication/app/src/main/java/com/bootcamp/emptyapplication/ListFragment.kt
@@ -1,15 +1,15 @@
 package com.bootcamp.emptyapplication
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.bootcamp.emptyapplication.adapters.PokemonAdapter
-import com.bootcamp.emptyapplication.databinding.FragmentListBinding
 import com.bootcamp.emptyapplication.models.Pokemon
 
 class ListFragment : Fragment(), PokemonListener {
@@ -33,6 +33,26 @@ class ListFragment : Fragment(), PokemonListener {
         val recycler: RecyclerView = view?.findViewById(R.id.recyclerList) as RecyclerView
         recycler.layoutManager = LinearLayoutManager(parentFragment?.context)
         recycler.adapter = adapter
+        adapter.notifyDataSetChanged()
+
+        val mSwipeRefreshLayout = view.findViewById<SwipeRefreshLayout>(R.id.swiperefresh)
+        mSwipeRefreshLayout.setOnRefreshListener {
+            addNewPokemon()
+            mSwipeRefreshLayout.isRefreshing = false
+        }
+    }
+
+    private fun addNewPokemon() {
+        pokemonList.clear()
+        pokemonList.add(Pokemon("Chicorita", "152", "90", "100"))
+        pokemonList.add(Pokemon("Bayleef", "153", "120", "100"))
+        pokemonList.add(Pokemon("Meganium", "154", "60", "100"))
+        pokemonList.add(Pokemon("Cyndaquil", "155", "60", "100"))
+        pokemonList.add(Pokemon("Quilava", "156", "60", "100"))
+        pokemonList.add(Pokemon("Typhlosion", "157", "60", "100"))
+        pokemonList.add(Pokemon("Totodile", "158", "60", "100"))
+        pokemonList.add(Pokemon("Croconaw", "159", "60", "100"))
+        pokemonList.add(Pokemon("Feraligatr", "160", "60", "100"))
         adapter.notifyDataSetChanged()
     }
 
