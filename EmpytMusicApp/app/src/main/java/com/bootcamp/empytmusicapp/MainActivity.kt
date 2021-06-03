9f5ba60f00b11816cc39f3df8eae1321771c396d
Andres.Arana <andres.arana@jala-foundation.org>
2021-06-03 14:31:45 -0400

    Service implemented

--- EmpytMusicApp/app/src/main/java/com/bootcamp/empytmusicapp/MainActivity.kt
+++ EmpytMusicApp/app/src/main/java/com/bootcamp/empytmusicapp/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.bootcamp.empytmusicapp
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bootcamp.empytmusicapp.adapters.SongAdapter
 import com.bootcamp.empytmusicapp.databinding.ActivityMainBinding
 import com.bootcamp.empytmusicapp.listeners.SongListener
 import com.bootcamp.empytmusicapp.models.Song
+import com.bootcamp.empytmusicapp.services.MusicService
 
 class MainActivity : AppCompatActivity(), SongListener {
     private lateinit var binding: ActivityMainBinding
@@ -26,9 +30,18 @@ class MainActivity : AppCompatActivity(), SongListener {
     }
 
     override fun playSong(song: Song) {
+        val intent = Intent(this, MusicService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent)
+        }
     }
 
     override fun stopSong(song: Song) {
+        val intent = Intent(this, MusicService::class.java)
+        intent.putExtra(MusicService.STOP_FLAG, true)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent)
+        }
     }
 
     override fun viewSongDetail(song: Song) {
--- EmpytMusicApp/app/src/main/java/com/bootcamp/empytmusicapp/services/MusicService.kt
+++ EmpytMusicApp/app/src/main/java/com/bootcamp/empytmusicapp/services/MusicService.kt
@@ -10,33 +10,38 @@ import com.bootcamp.empytmusicapp.MainActivity
 import com.bootcamp.empytmusicapp.R
 
 class MusicService: Service() {
+    companion object {
+        const val STOP_FLAG = "STOP_FLAG"
+        const val SERVICE_ID = 3
+    }
     override fun onBind(p0: Intent?): IBinder? {
         return null
     }
-
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        val pendingIntent = Intent(this, MainActivity::class.java).let {
-            PendingIntent.getActivity(this, 0, it, 0)
+        val stop = intent?.getBooleanExtra(STOP_FLAG, false) ?: false
+        if (stop) {
+            stopForeground(true)
+        } else {
+            val pendingIntent = Intent(this, MainActivity::class.java).let {
+                PendingIntent.getActivity(this, 0, it, 0)
+            }
+            registerChannel(this)
+            val notification = NotificationCompat.Builder(this, "musicapp")
+                .setContentTitle("Music")
+                .setContentText("Music App")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentIntent(pendingIntent)
+                .build()
+            startForeground(SERVICE_ID, notification)
         }
-
-        registerChannel(this)
-
-        val notification = NotificationCompat.Builder(this, "musicapp")
-            .setContentTitle("Music")
-            .setContentText("Music App")
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setSmallIcon(R.drawable.ic_launcher_background)
-            .setContentIntent(pendingIntent)
-            .build()
         return START_STICKY
     }
-
     private fun registerChannel(context: Context) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel("musicapp", "musicapp", NotificationManager.IMPORTANCE_LOW)
             channel.description = "Description"
             channel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
-
             val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
         }
