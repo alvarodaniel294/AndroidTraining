545ebdd606992335f36b73474200c15c8e7d14c0
christian torrico <christorricoavila@gmail.com>
2021-06-03 07:55:16 -0400

    log

--- EmpytMusicApp/app/src/main/java/com/bootcamp/empytmusicapp/MainActivity.kt
+++ EmpytMusicApp/app/src/main/java/com/bootcamp/empytmusicapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.bootcamp.empytmusicapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("chris","hola christian")
     }
 }
