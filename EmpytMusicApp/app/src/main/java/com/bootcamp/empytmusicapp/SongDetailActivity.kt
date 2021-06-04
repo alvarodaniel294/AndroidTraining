828fb0a4a198202ca9b4394c98121147a1457d9a
Andres.Arana <andres.arana@jala-foundation.org>
2021-06-04 15:47:13 -0400

    Song detail implemented

--- EmpytMusicApp/app/src/main/java/com/bootcamp/empytmusicapp/SongDetailActivity.kt
+++ EmpytMusicApp/app/src/main/java/com/bootcamp/empytmusicapp/SongDetailActivity.kt
@@ -2,13 +2,18 @@ package com.bootcamp.empytmusicapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bootcamp.empytmusicapp.databinding.ActivitySongDetailBinding
 
 class SongDetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySongDetailBinding
     companion object {
         const val SONG_EXTRA = "SONG_EXTRA"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_song_detail)
+        binding = ActivitySongDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.songName
     }
 }
